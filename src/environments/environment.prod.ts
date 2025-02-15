0756efa75cd83b8f75c3151ee1e1ace430f76f91
Ravan Ismayilov <rvanismayilov70@gmail.com>
2025-02-16 00:09:53 +0400

    commit 6

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: true,
+    API_URL: "https://my-portfolio-back-49v7.onrender.com/api"
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: false,
+    API_URL: "https://my-portfolio-back-49v7.onrender.com/api"
+};
